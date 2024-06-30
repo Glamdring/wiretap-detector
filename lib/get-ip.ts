f2ee9c35267bca0014787e24163090680ac04689
bozho-logsentinel <bozhidar.bozhanov@logsentinel.com>
2024-06-30 14:34:27 +0300

    using fetch-retry to work around temporary network failures

--- lib/get-ip.ts
+++ lib/get-ip.ts
@@ -4,7 +4,13 @@ type IpifyResponse = {
 export type IP = string;
 
 export const getIp = async (): Promise<IP> => {
-  const response: Response = await fetch('https://api.ipify.org/?format=json');
+  const fetch = require('fetch-retry')(global.fetch);
+  const response: Response = await fetch('https://api.ipify.org/?format=json', {
+    retries: 3,
+    retryDelay: function(attempt, error, response) {
+      return Math.pow(2, attempt) * 3000; // 3000, 6000, 12000
+    }
+  });
   const ipifyResponse: IpifyResponse = await response.json();
 
   return ipifyResponse.ip;
